8845e0a6ec6cbaaccf63785db2cacc94f2520bfb
Federico Squartini <federico.squartini@gmail.com>
2017-03-25 13:17:54 +0100

    Change bundle output dir to public

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
