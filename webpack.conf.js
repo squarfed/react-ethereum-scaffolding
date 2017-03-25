ccc44db7b491c8fd3e4f0f79379d1639278f76e7
Federico Squartini <federico.squartini@gmail.com>
2017-03-25 12:03:38 +0100

    Add basic webpack config file

---
+++ webpack.conf.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: __dirname,
+    filename: './dist/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  }
+};
