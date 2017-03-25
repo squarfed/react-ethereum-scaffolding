7cc6f105bfe6c358874b96b044a360ce6c3a1fc5
Federico Squartini <federico.squartini@gmail.com>
2017-03-25 13:12:57 +0100

    First commit hello world component

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <h1> Hello world </h1>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
+
