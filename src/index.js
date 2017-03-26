a56446364ee7d4d6a16ea1210b90b1dbca51197e
Federico Squartini <federico.squartini@gmail.com>
2017-03-26 18:41:51 +0200

    Add sass bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './App.scss'
 
 const App = () => {
   return (
-    <h1> Hello world </h1>
+    <h1> Hello world! </h1>
   )
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'))
 
